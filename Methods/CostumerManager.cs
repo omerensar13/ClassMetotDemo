6a55c0b9527f3008f5ff239c03e4f8786ea756b1
omerensar13 <omerensar@hotmail.com.tr>
2021-01-19 12:39:14 +0100

    Demo

---
+++ Methods/CostumerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodsDemo
+{
+    class CostumerManager
+    {
+        public void  AddCostumer (Costumer costumer)
+        {
+            Console.WriteLine(costumer.Name + "  " + costumer.Surname + " sisteme eklendi!  ");
+        }
+        public void DeleteCostumer (Costumer costumer)
+        {
+            Console.WriteLine(costumer.Name + "  " + costumer.Surname + " sistemden silindi!  ");
+        }
+        public void ListCostumer (Costumer[] costumers)
+        {
+
+            foreach (var costumer in costumers)
+            {
+                Console.WriteLine(costumer.Id);
+                Console.WriteLine(costumer.Name);
+                Console.WriteLine(costumer.Surname);
+                Console.WriteLine(costumer.PhoneNumber);
+                Console.WriteLine(costumer.Adress);
+            }
+
+         }
+
+
+    
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMethodsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Costumer costumer1 = new Costumer();
+            costumer1.Id = 1;
+            costumer1.Name = "Adem";
+            costumer1.Surname = "Oğlu";
+            costumer1.PhoneNumber = 05366987898;
+
+            Costumer costumer2 = new Costumer();
+            costumer2.Id = 2;
+            costumer2.Name = "Havva";
+            costumer2.Surname = "Oğlu";
+            costumer2.PhoneNumber = 0536698995;
+
+            Costumer costumer3 = new Costumer();
+            costumer3.Id = 3;
+            costumer3.Name = "Habil";
+            costumer3.Surname = "Oğlu";
+            costumer3.PhoneNumber = 05866927818;
+
+            Costumer[] costumers = new Costumer[] {costumer1,costumer2,costumer3 };
+
+            CostumerManager costumerManager = new CostumerManager();
+            costumerManager.AddCostumer(costumer1);
+            costumerManager.AddCostumer(costumer2);
+            costumerManager.AddCostumer(costumer3);
+            costumerManager.ListCostumer(costumers);
+            costumerManager.DeleteCostumer(costumer1);
+            costumerManager.DeleteCostumer(costumer2);
+            costumerManager.DeleteCostumer(costumer3);
+
+
+
+
+        }
+    }
+}
